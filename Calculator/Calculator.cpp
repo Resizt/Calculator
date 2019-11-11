28f33b2dfe92bf110f190994253f3d5ce2d23834
Genesis Jackson <resiztdzn@gmail.com>
2019-11-11 08:45:30 -0500

    Added mutiple functions
    As i revisted this program, I added trig functions that could be done with a single input. I'm going to add a feature later that stores and keep your answer until you "Clear" it like a real calculator.

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 
 // Inputs
-int numbers			(int&, int&, int&);
+int numbers			(int&, int&, double&);
 void message		(int&);
-void postmessage	(int&, int&, int&, int&);
+void postmessage	(int&, int&, int&, double&);
+void trigfunctions	(int&, double&);
 
 // Functions
-int addition		(int&, int&, int&);
-int subtraction		(int&, int&, int&);
-int multiplication	(int&, int&, int&);
-int divison			(int&, int&, int&);
+int addition		(int&, int&, double&);
+int subtraction		(int&, int&, double&);
+int multiplication	(int&, int&, double&);
+int divison			(int&, int&, double&);
 
 // System message
 void error();
@@ -20,24 +21,31 @@ void error();
 /*-------------------------------------------------------------------------------------*/
 
 int main() {
-	int choice, num1, num2, sum;
+	int choice, num1, num2;
+	double sum;
 
 	message(choice);
 
 	while (true) {
-		numbers(num1, num2, sum);
 		switch (choice) {
 		case 1:
+			numbers(num1, num2, sum);
 			addition(num1, num2, sum);
 			break;
 		case 2:
+			numbers(num1, num2, sum);
 			subtraction(num1, num2, sum);
 			break;
 		case 3:
+			numbers(num1, num2, sum);
 			multiplication(num1, num2, sum);
 			break;
 		case 4:
+			numbers(num1, num2, sum);
 			divison(num1, num2, sum);
+			break;
+		case 5:
+			trigfunctions(choice, sum);
 			break;
 		}
 		postmessage(choice, num1, num2, sum);
@@ -49,12 +57,12 @@ int main() {
 void message(int& choice) {
 	cout << "Welcome to the terminal Calculator" << endl;
 	cout << "Choose an option from below" << endl;
-	cout << " 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Divison " << endl;
+	cout << " 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Divison \n 5. Trig Functions" << endl;
 
 	cin >> choice;
 
 	// Input Validator
-	while (0 > choice || choice > 4) {
+	while (0 > choice || choice > 5) {
 		error();
 		cin >> choice;
 	}
@@ -62,16 +70,57 @@ void message(int& choice) {
 	cout << endl << endl;
 }
 
-void postmessage(int& choice, int& a, int& b, int& c) {
+void trigfunctions(int& choice, double& sum){
+	system("CLS");
+	cout << "Choose an trig function from below" << endl;
+	cout << " 1. Sin \n 2. Cos \n 3. Tan \n 4. ArcSin \n 5. ArcCos \n 6. ArcTan" << endl;
+	int x;
+	cin >> choice;
+
+	switch (choice) {
+	case 1:
+		cout << "Input the sin function" << endl;
+		cin >> x;
+		sum = sin(x);
+		break;
+	case 2:
+		cout << "Input the Cos function" << endl;
+		cin >> x;
+		sum = cos(x);
+		break;
+	case 3:
+		cout << "Input the Tan function" << endl;
+		cin >> x;
+		sum = tan(x);
+		break;
+	case 4:
+		cout << "Input the ArcSin function" << endl;
+		cin >> x;
+		sum = asin(x);
+		break;
+	case 5:
+		cout << "Input the ArcCos function" << endl;
+		cin >> x;
+		sum = acos(x);
+		break;
+	case 6:
+		cout << "Input the ArcTan function" << endl;
+		cin >> x;
+		sum = atan(x);
+		break;
+	}
+}
+
+void postmessage(int& choice, int& a, int& b, double& c) {
 	system("CLS");
 	cout << "Your Answer: " << c << endl;
 	cout << "---------------------------------" << endl;
-	cout << " 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Divison " << endl;
+	cout << " 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Divison \n 5. Trig Functions" << endl;
 
 	cin >> choice;
 
 	// Input Validator
-	while (0 > choice || choice > 4) {
+	while (0 > choice || choice > 5) {
 		error();
 		cin >> choice;
 	}
@@ -81,7 +130,7 @@ void postmessage(int& choice, int& a, int& b, int& c) {
 
 /*-------------------------------------------------------------------------------------*/
 
-int numbers(int& a, int& b, int& c) {
+int numbers(int& a, int& b, double& c) {
 	cout << "Please choose two number you want to compute" << endl;
 
 	cout << "Number 1: ";
@@ -93,25 +142,25 @@ int numbers(int& a, int& b, int& c) {
 	return c;
 }
 
-int addition(int& a, int& b, int& c) {
+int addition(int& a, int& b, double& c) {
 	c = a + b;
 	cout << c << endl;
 	return c;
 }
 
-int subtraction(int& a, int& b, int& c) {
+int subtraction(int& a, int& b, double& c) {
 	c = a - b;
 	cout << c << endl;
 	return c;
 }
 
-int multiplication(int& a, int& b, int& c) {
+int multiplication(int& a, int& b, double& c) {
 	c = a * b;
 	cout << c << endl;
 	return c;
 }
 
-int divison(int& a, int& b, int& c) {
+int divison(int& a, int& b, double& c) {
 	c = a / b;
 	cout << c << endl;
 	return c;
